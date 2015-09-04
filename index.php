3bb4ab5e2efc21492fc032e7fe2f225de0003106
FabianoFaria <fabiano@joiss.com.br>
2015-09-04 01:18:47 -0300

    primeiro commit projeto

---
+++ index.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+	<head>
+
+		<title>
+				Rei Arthur Hamburgueria
+		</title>
+
+		<script type="text/javascript" src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+
+		<link href="960.min.css" media="screen" rel="stylesheet" type="text/css" />
+	
+		<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+
+		<link rel="icon" href="imagens/favicon.png" type="image/png" />
+		
+				
+		<script type="text/javascript" src="js/namespace.js"></script>
+		<script type="text/javascript" src="js/widget.loader.js"></script>
+		<script type="text/javascript" src="js/jquery.json.min.js"></script>
+		<script type="text/javascript" src="js/jquery.ajaxq.min.js"></script>
+		
+
+		<script type="text/javascript" src="js/jquery.slides.min.js"></script>	
+
+	</head>
+
+	<body>
+
+		<div id="wrapper">
+
+
+			<div id="header">
+
+				<div class="container_12 headerRelative">
+
+					<h1>
+						<a>Rei Arthur Hanburgueria</a>
+					</h1>
+
+					<div id="navContainer">
+
+						<ul class="ul-1">
+
+							<li class="li-separator">
+								<a>A hamburgueria </a>
+							</li>
+							<li >
+								<a>Cardapios </a>
+							</li>
+
+						</ul> <!-- ul-1 -->
+
+						<ul class="ul-2">
+
+							<li class="li-separator">
+								<a>Shows </a>
+							</li>
+							<li >
+								<a>Contato </a>
+							</li>
+
+						</ul> <!-- ul-2 -->
+
+					</div> <!-- navContainer -->
+
+
+				</div> <!-- container_12 headerRelative -->
+
+			</div> <!-- header -->
+
+			<style type="text/css">
+				#container{
+					width:100% !important;
+				}
+				
+				#header{
+					background: none !important;
+				}
+				
+				.flash{
+				display:none;
+				}
+				
+				.animate2{
+					position: absolute;
+					background: url(http://www.quiosquechoppbrahma.com.br/skins/default/default/images/homeBanner3.png);
+				}
+				
+				#bannerContainer{
+					height:420px;
+				}
+
+				#txt1{
+					background-position: 0 0;
+					top: 0;
+					width: 804px;
+					height: 120px;	
+					display:none;
+				}
+				
+				#txt2{
+					background-position: 0 -127px;
+					top: 139px;
+					width: 804px;
+					height: 120px;	
+					display:none;
+					cursor:pointer;
+				}
+				
+				#txt3{
+					background-position: 0 -256px;
+					top: 267px;
+					width: 804px;
+					height: 120px;	
+					display:none;
+				}
+
+				#txt4{
+					background-position: 0 -376px;
+					top: 385px;
+					width: 804px;
+					height: 30px;
+					display:none;
+				}
+				
+				.pagination{
+					position: absolute;
+					bottom: 20px;
+					right: 50%;
+					z-index: 5;
+					margin-right: -450px;
+				}
+				
+				.pagination li{
+					float:left;
+				}	
+				
+				.pagination a{
+					background-color: #8B1D0B;
+					color: white;
+					padding: 4px;
+					border-radius: 16px;
+					display: block;
+					width: 13px;
+					padding-left: 9px;
+					margin-left: 10px;
+				}
+				
+				.pagination li.current a{
+					background-color:#f4c50a;
+					color:#8b1d0b;
+				}
+				
+				#bannerContainer .inner{
+					width: 1280px;
+					height: 570px;
+					position: absolute;
+					top: -156px;
+					left: 50%;
+					margin-left: -640px;
+					z-index: 0;
+				}
+				
+				#bannerContainer .slides_container{
+					width: 1280px;
+					height: 570px;
+				}
+				
+				#bannerContainer .slides_container div{
+					position:relative;
+				}
+				
+				#bannerContainer .slides_container .link1{
+					display: block;
+					position: absolute;
+					top: 368px;
+					left: 191px;
+					width: 101px;
+					height: 27px;
+					z-index:10;
+				}
+
+				#bannerContainer .slides_container .link3{
+					display: block;
+					position: absolute;
+					top: 525px;
+					left: 840px;
+					width: 110px;
+					height: 30px;
+					z-index:10;
+				}
+				
+				#bannerContainer .slides_container .link2{
+					display: block;
+					position: absolute;
+					top: 400px;
+					left: 191px;
+					width: 101px;
+					height: 27px;
+					z-index:10;
+				}
+
+				#bannerContainer .slides_container .link3{
+					display: block;
+					position: absolute;
+					top: 0px;
+					left: 0px;
+					width: 1280px;
+					height: 520px;
+					z-index:10;
+				}
+				
+				#bannerContainer .slides_container .linkOktober{
+					display: block;
+					position: absolute;
+					top: 510px;
+					left: 601px;
+					width: 77px;
+					height: 21px;
+					z-index:10;
+				}
+
+				#oktoberfest #regulamento{
+					display: block;
+					width: 114px;
+					height: 28px;
+					position: absolute;
+					bottom: 12px;
+					right: 308px;
+				}
+				
+				.regulamentoOktober{
+					width: 500px;
+					height: 510px;
+					margin-top: 0px;
+					margin-left: 0px;
+					overflow: auto;
+				}
+
+				.regulamentoOktober p{
+					margin-bottom: 10px;
+				}
+				
+				.regulamentoOktober .title{
+					font-family: 'TradeGothicLTStdBdCnNo.20';
+					font-size:26px;
+					color:#AF1804;
+				}
+
+				.regulamentoOktober #moderacao{
+					border: 1px solid #000;
+					width: 400px;
+					height: 47px;
+					padding: 5px;
+					text-align: center;
+					margin-left: 80px;
+					margin-top: 20px;
+					margin-bottom: 20px;
+				}
+				.regulamentoOktober #anexo{
+					text-decoration: underline;
+					color: blue;
+				}
+				#bannerContainer .slides_container .link5 {
+					display: block;
+					position: absolute;
+					top: 464px;
+					left: 61px;
+					width: 221px;
+					height: 47px;
+					z-index: 10
+				}
+
+
+			</style>
+
+
+
+
+			<script src="js/slides.min.jquery.js"></script>
+
+			<script type="text/javascript" src="js/jquery.fancybox.js?v=2.1.5"></script>
+			<link rel="stylesheet" type="text/css" href="js/jquery.fancybox.css?v=2.1.5" media="screen" />
+
+			<script type="text/javascript">
+				$(function(){
+					$('.inner').slides({
+						preload: false,
+						generateNextPrev: false
+					});
+				});
+			</script>
+
+			<div id="container" class="container_12">
+
+				<div id="bannerContainer" class="container_12">
+
+
+					<div class="inner">
+
+						<div class="slides_container" style="overflow: hidden; position: relative; display: block;">
+
+							
+
+								<div style='background-image: url("imagens/bannerHome1b.jpg"); height: 570px; width: 1280px; position: absolute; top: 0px; left: 1280px; z-index: 0; display: none;'>
+									<a class="link1" href="/cardapio"></a>
+								</div> <!-- background-image -->
+
+								<div style='background-image: url("imagens/bannerHome1c.jpg"); height: 570px; width: 1280px; position: absolute; top: 0px; left: 1280px; z-index: 0; display: none;'>
+									<a class="link1" href="/quiosque?convide=1"></a>
+								</div> <!-- background-image -->
+
+								<div style='background-image: url("imagens/bannerHome1d.jpg"); height: 570px; width: 1280px; position: absolute; top: 0px; left: 1280px; z-index: 0; display: none;'>
+									<a class="link2" href="/especialistas"></a>
+								</div> <!-- background-image -->
+
+
+						</div>
+						
+
+					</div> <!-- inner -->	
+
+
+				</div> <!-- bannerContainer -->
+
+				<div class="container_12">
+					<h3>Card&aacute;pios do Rei Arthur</h3>
+
+					<div class="cardapio-almoco"><a href="#" title="Card&aacute;pio de Almo&ccedil;o"><span>almo&ccedil;o</span></a></div>
+					<div class="cardapio-happy"><a href="#" title="Card&aacute;pio de Happy Hour"><span>happy hour</span></a></div>
+					<div class="cardapio-jantar"><a href="#" title="Card&aacute;pio de Jantar"><span>jantar</span></a></div>
+					<div class="cardapio-cervejas"><a href="#" title="Card&aacute;pio de Cervejas"><span>cervejas</span></a></div>
+					<div class="cardapio-destilados"><a href="#" title="Card&aacute;pio de Destilados"><span>destilados</span></a></div>
+					<div class="cardapio-cafes"><a href="#" title="Card&aacute;pio de Caf&eacute;es"><span>caf&eacute;s</span></a></div>
+
+				</div> <!-- container_12 -->
+
+				<div id="footer">
+
+					<footer class="centered footer2">
+						<div class="linha-footer">
+							<h3>Hor&aacute;rios da Casa</h3>
+							<div class="box">
+								<h2>Cafeteria</h2>
+								<p>Segunda a S&aacute;bado a partir das 11h</p>
+							</div>
+							<div class="box">
+								<h2>Almo&Ccedil;o</h2>
+								<p>Segunda a S&aacute;bado das 11:30h &agrave;s 15h</p>
+							</div>
+							<div class="box">
+								<h2>Happy Hour</h2>
+								<p>Ter&ccedil;a a Sexta das 18h &agrave;s 20:30h</p>
+							</div>
+							<div class="box">
+								<h2>Jantar</h2>
+								<p>Ter&ccedil;a a Sexta a partir das 18h</p>
+								<p>S&aacute;bados a partir das 20h</p>
+							</div>
+							<div class="box">
+								<h2>Jokers Pub</h2>
+								<p>Ter&ccedil;a a Sexta a partir das 18h</p>
+								<p>S&aacute;bados a partir das 20h</p>
+							</div>
+						</div>
+
+						<div class="linha-footer">
+							<h3>Mapa do site</h3>
+							<nav class="nav-footer">
+							<ul>
+								<li><a href="http://www.jokers.com.br/jokers/" title="Conhe&ccedil;a o Jokers" class="jokers"></a></li>
+								<li><a href="http://www.jokers.com.br/agendas/" title="Agenda de Eventos" class="agenda"></a></li>
+								<li><a href="http://www.jokers.com.br/servicos/" title="Nossos Servi&ccedil;os" class="servicos"></a></li>
+								<li><a href="http://www.jokers.com.br/blog/" target="_blank" title="Blog" class="news"></a></li>
+								<li><a href="http://www.jokers.com.br/promocoes/" title="Nossas Promo&ccedil;&otilde;es" class="promocoes"></a></li>
+								<li><a href="http://www.jokers.com.br/albuns/" title="Galeria Jokers" class="galeria"></a></li>
+								<li><a href="http://www.jokers.com.br/cardapios/" title="Card&aacute;pio" class="cardapio"></a></li>
+								<li><a href="http://www.jokers.com.br/jukebox/" title="Jukebox Jokers" class="jukebox"></a></li>
+								<li><a href="http://www.jokers.com.br/contato/" title="Entre em contato conosco" class="contato"></a></li>
+							</ul>
+							</nav>
+						</div>
+
+						<h1 class="logo-footer"><a href="http://www.jokers.com.br/"><img src="imagens/topo.png" alt="Logo Rei Arthur" title="Rei Arthur" /></a></h1>
+
+						<div class="endereco">
+							<nav class="nav-social">
+							<ul>
+							<li><a href="#" target="_blank" title="Curta nossa p&aacute;gina no Facebook" class="facebook"></a></li>
+							<li><a href="#" target="_blank" title="Siga-nos no Twitter" class="twitter"></a></li>
+							<li><a href="#" target="_blank" title="Veja nossos v&iacute;deos no YouTube" class="youtube"></a></li>
+							</ul>
+							</nav>
+							<p>R. S&atilde;o Francisco, 164, Centro. Curitiba - PR | jokers@jokers.com.br</p>
+							<p>Telefone  (41) 3324-2351 | <strong>Reservas  (41) 3013-5164</strong></p>
+							<img src="imagens/cartoes.png" alt="Cart&otilde;es" title="Aceitamos os cart&otilde;es Mastercard, Visa, American Express, Dinners Club, Hipercard, Ticket Alimenta&ccedil;&atilde;o, Sodexo, Redecard e Elo" />
+						</div>
+
+						<div class="parceiros">
+							<h3>Parceiros</h3>
+							<img src="imagens/logo-parceiros.png" alt="Parceiros Jokers" title="Eisenbahn - Heineken - Jagermeister">
+						</div>
+					</footer>
+
+				</div>
+
+				<div id="assinatura">
+					<div class="centered">
+						<h3>Rei Arthur Hamburgueria 2015 &reg; todos os direitos reservados</h3>
+					</div>
+				</div>
+
+			</div> <!-- id container -->
+
+
+		</div>
+
+		
+	</body>
+
+</html>
